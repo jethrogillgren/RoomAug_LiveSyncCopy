943530cacded3fd8c095efa1ab8657496007f1dc
jethrogillgren <jethrogillgren@gmail.com>
2018-03-06 09:15:11 +0000

    Mod script

--- SetupLiveSync.sh
+++ SetupLiveSync.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+echo "This will symlink all subfolders of the specific assets folder, excluding the hardcoded folder names.  You should either ./RemovePlatformExcludes, or manually check the PlatformSpecific symlinks after running htis script."
+
 read -p "Target Assets folder (../RoomAug/Assets):" base
 base=${base:-../RoomAug/Assets}
 
@@ -19,4 +21,4 @@ for file in ${base}/*; do
 done
 
 
-echo "You might want to copy the ProjectSettings folder across"
+echo "You might want to copy the ProjectSettings folder across, and manually symlink any PlatformSpecific dirs you need"
